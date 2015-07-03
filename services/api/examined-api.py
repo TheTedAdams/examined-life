e7f972645057bef923aedd4138c7653b4c4b20f9
enriched <feelingenriched@gmail.com>
2015-07-03 02:30:31 -0600

    Adding some more structure to the services

--- services/api/examined-api.py
+++ services/api/examined-api.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def api_root():
-    return 'Hello world'
+    return 'Hello from the analytics api'
+
+
+@app.route('/oauth2/token')
+def get_oauth2_token():
+    return 'Did you really think I would give you one?'
+
+
+@app.route('/oauth2/authorize')
+def authorize_oauth2_token():
+    return 'Nope, you\'re not, I guarantee it'
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
