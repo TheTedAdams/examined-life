4b4c0b12ee03ee7dca52b02540570605ceada173
enriched <feelingenriched@gmail.com>
2015-07-03 15:33:47 -0600

    Better if it's relative to the project

--- services/expyapi/app.py
+++ services/expyapi/app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 from flask_restful import Api
-from resources.oauth2 import Oauth2
+from .resources.oauth2 import Oauth2
 
 app = Flask(__name__)
 api = Api(app)
-
 api.add_resource(Oauth2, '/oauth2')
